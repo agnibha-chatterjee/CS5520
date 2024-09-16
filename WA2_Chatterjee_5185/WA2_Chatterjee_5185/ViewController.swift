52382fda9bc0d7ee4bab82fd0b4f4f381a0a5699
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-09-15 18:29:46 -0700

    inital commit for assignment 2

---
+++ WA2_Chatterjee_5185/WA2_Chatterjee_5185/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WA2_Chatterjee_5185
+//
+//  Created by agni on 9/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
