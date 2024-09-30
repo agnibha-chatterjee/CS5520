5a637db82216dfb25a2e9e62a0abceeebd58da26
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-09-29 22:31:23 -0700

    a4-populated table with dummy data

--- WA4_Chatterjee_5185/WA4_Chatterjee_5185/MainScreen/MainScreenViewController.swift
+++ WA4_Chatterjee_5185/WA4_Chatterjee_5185/MainScreen/MainScreenViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class MainScreenViewController: UIViewController {
     
     let mainScreenView = MainScreenView()
+    var contacts = [Contact]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +21,48 @@ class MainScreenViewController: UIViewController {
             barButtonSystemItem: .add, target: self,
             action: #selector(onAddBarButtonTapped)
         )
+        
+        contacts.append(Contact(name: "Agni",
+                                email: "agni@agni.com",
+                                phoneType: "Cell",
+                                phoneNumber: "9901199218",
+                                address: "601 William St",
+                                cityState: "Oak",
+                                zip: "94612"))
+        contacts.append(Contact(name: "Sruti",
+                                email: "s@s.com",
+                                phoneType: "Work",
+                                phoneNumber: "425654221",
+                                address: "435 William St",
+                                cityState: "New Port",
+                                zip: "92612"))
+        
+        mainScreenView.tableViewContacts.dataSource = self
+        mainScreenView.tableViewContacts.delegate = self
     }
     
     @objc func onAddBarButtonTapped() {
           
     }
+    
+}
 
-
+extension MainScreenViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contacts", for: indexPath) as! TableViewContactCell
+        let idx = indexPath.row
+        cell.labelName.text = contacts[idx].name
+        cell.labelEmail.text = contacts[idx].email
+        let phoneNumber = contacts[idx].phoneNumber
+        let phoneType = contacts[idx].phoneType
+        cell.labelPhoneInfo.text = "\(phoneNumber) (\(phoneType))"
+        return cell
+    }
+    
+    
 }
 
