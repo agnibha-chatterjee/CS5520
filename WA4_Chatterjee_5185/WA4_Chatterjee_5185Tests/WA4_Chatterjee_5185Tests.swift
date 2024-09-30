0f2e94201d337e3e554d74053d0bda31d5107876
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-09-29 21:42:46 -0700

    created assignment 4

---
+++ WA4_Chatterjee_5185/WA4_Chatterjee_5185Tests/WA4_Chatterjee_5185Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WA4_Chatterjee_5185Tests.swift
+//  WA4_Chatterjee_5185Tests
+//
+//  Created by agni on 9/29/24.
+//
+
+import Testing
+@testable import WA4_Chatterjee_5185
+
+struct WA4_Chatterjee_5185Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
