a62e802a77e89e8de05cedd3aebd18639b9bdbf2
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-11-03 00:00:42 -0700

    add assignment 7

---
+++ WA7_Chatterjee_5185/WA7_Chatterjee_5185/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WA7_Chatterjee_5185
+//
+//  Created by agni on 11/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WA7_Chatterjee_5185/WA7_Chatterjee_5185Tests/WA7_Chatterjee_5185Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WA7_Chatterjee_5185Tests.swift
+//  WA7_Chatterjee_5185Tests
+//
+//  Created by agni on 11/2/24.
+//
+
+import Testing
+@testable import WA7_Chatterjee_5185
+
+struct WA7_Chatterjee_5185Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
